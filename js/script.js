aaca729b270f040ddbb6c04de45ec9af553b6234
kalle <kmthorsnes@gmail.com>
2018-08-08 16:32:03 +0200

    wip

--- js/script.js
+++ js/script.js
@@ -1,58 +1,51 @@
 // FSJS - Random Quote Generator
 // Adds silly line for test
-var randomNumber = Math.ceil(quotes.length);
+
+
+
 
 // Create the array of quote objects and name it quotes
-var quotes = [
-    {
-    quote: "This is my quote, there are many like it, but this is mine.",
-    source: "Karl-Magnus Thorsnes",
-    citation: "Voices in head",
-    year: 2018
+var quotes = [{
+        quote: "This is my quote, there are many like it, but this is mine.",
+        source: "Karl-Magnus Thorsnes",
+        citation: "Voices in head"
     },
-{
-    quote: "Great minds discuss ideas; average minds discuss events; small minds discuss people.",
-    source: "Eleanor Roosevelt",
-    citation: null,
-    year: 2018
-},
-{
-    quote: "Jerry, just remember, it's not a lie if you believe it.",
-    source: "George Costanza",
-    citation: "Seinfeld S06E15",
-    year: 1995
-},
-{
-    quote: "But man is not made for defeat. A man can be destroyed but not defeated.",
-    source: "Ernest Hemingway",
-    citation: null,
-    year: null
-},
-{
-    quote: "Learning never exhausts the mind.",
-    source: "Leonardo da Vinci",
-    citation: null,
-    year: "2"
-},
-{
-    quote: "Wise men speak because they have something to say; Fools because they have to say something.",
-    source: "Plato",
-    citation: null,
-    year: null
-}
+    {
+        quote: "Great minds discuss ideas; average minds discuss events; small minds discuss people.",
+        source: "Eleanor Roosevelt",
+        citation: "kuk"
+    },
+    {
+        quote: "Jerry, just remember, it's not a lie if you believe it.",
+        source: "George Costanza",
+        citation: "Seinfeld S06E15"
+    },
+    {
+        quote: "But man is not made for defeat. A man can be destroyed but not defeated.",
+        source: "Ernest Hemingway",
+        citation: "kuk"
+    },
+    {
+        quote: "Learning never exhausts the mind.",
+        source: "Leonardo da Vinci",
+        citation: "kuk"
+    },
+    {
+        quote: "Wise men speak because they have something to say; Fools because they have to say something.",
+        source: "Plato",
+        citation: "kuk"
+    }
 ];
 
 
 // Create the getRandomQuuote function and name it getRandomQuote
-function getRandomQuuote() 
-    {
+function getRandomQuuote() {
 
-    };
+};
 
 
-// Create the printQuote funtion and name it printQuote
-function printQuote() 
-{
+// Create the printQuote function and name it printQuote
+function printQuote() {
 
 };
 
@@ -60,3 +53,6 @@ function printQuote()
 // This event listener will respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+var randomNumber = Math.floor(Math.random() * quotes.length );
+console.log(randomNumber);
